260c54c9cf19b74dcf1c727e2f4b51026e19fef2
ccficorilli <ccficorilli@gmail.com>
2018-09-28 13:17:41 -0700

    finally uploading

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:create-todo="addTodo"></create-todo>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  data() {
+    return {
+      todos: [
+        { title: 'Todo A', project: 'Project A', done: false },
+        { title: 'Todo B', project: 'Project B', done: true },
+        { title: 'Todo C', project: 'Todo C', done: false },
+        { title: 'Todo D', project: 'Todo D', done: false },
+      ],
+    };
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.push(
+        todo
+      );
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
